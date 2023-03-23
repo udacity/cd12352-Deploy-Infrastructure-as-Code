204aa295745b04481e98c88a80b24684caf92d50
fernandosilvacornejo <fernandosilvacornejo@users.noreply.github.com>
2023-03-23 12:32:47 -0300

    Correct duplicate resource

--- servers-and-security-groups/create.sh
+++ servers-and-security-groups/create.sh
@@ -2,4 +2,4 @@ aws cloudformation create-stack --stack-name $1 \
     --template-body file://$2   \
     --parameters file://$3  \
     --capabilities "CAPABILITY_NAMED_IAM"  \
-    --region=us-west-2
+    --region=us-east-1
